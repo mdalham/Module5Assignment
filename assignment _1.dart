5befbdf12644f18e32f5f7b6e041f4104213d56c
Md Alham Shano Aside <mdalham003@gmail.com>
2025-09-10 23:07:12 +0600

    assignment _1.dart

--- assignment _1.dart
+++ assignment _1.dart
@@ -10,29 +10,31 @@ class Book {
   double discountedPrice(double discountPercent) {
     double discountedAmount = price * (discountPercent / 100);
     double finalPrice = price - discountedAmount;
+    return finalPrice;
+  }
+
+  void bookDetails(double discountPercent) {
+    double finalPrice = discountedPrice(discountPercent);
+    double discountedAmount = price - finalPrice;
 
     //Print book details
     print('Book Name: $title');
     print('Author Name: $author');
     print('Original Price of Book: ${price.toStringAsFixed(0)} Taka');
-    print(
-      'Discount: $discountPercent% or ${discountedAmount.toStringAsFixed(0)} Taka',
-    );
+    print('Discount: $discountPercent% or ${discountedAmount.toStringAsFixed(0)} Taka');
     print('Discounted Price: ${finalPrice.toStringAsFixed(0)} Taka\n');
-
-    return finalPrice;
   }
 }
 
 //Main function
 void main() {
-  //Book instances
+  //Book instances(details)
   Book book1 = Book('Accounting Principles', 'Jerry J. Weygandt', 850.0);
-  book1.discountedPrice(12.8);
+  book1.bookDetails(12.8);
 
   Book book2 = Book('Principles of Economics', 'N. Gregory Mankiw', 600.0);
-  book2.discountedPrice(15.0);
+  book2.bookDetails(15.0);
 
   Book book3 = Book('English Grammar & Composition', 'Jahirul Islam', 385.0);
-  book3.discountedPrice(8.5);
+  book3.bookDetails(8.5);
 }
